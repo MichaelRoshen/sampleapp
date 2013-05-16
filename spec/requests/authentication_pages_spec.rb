d6e072d4f68fc875322833dbfd0d328609bc70a1
MichaelRoshen <michael_roshen@163.com>
2013-05-16 23:13:01 +0800

    finish edit user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,7 +18,7 @@ describe "AuthenticationPages" do
 
       describe "after visiting another page" do
         before { click_link "Home" }
-	it { should_not have_selector("div.alert.alert-error")}
+        it { should_not have_selector("div.alert.alert-error")}
       end
     end
 
@@ -26,11 +26,12 @@ describe "AuthenticationPages" do
       let(:user) {FactoryGirl.create(:user) }
       before do 
         fill_in "Email", with: user.email
-	fill_in "Password", with: user.password
-	click_button "Sign In"
+        fill_in "Password", with: user.password
+        click_button "Sign In"
       end
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Setting', href: edit_user_path(user)) }
       it { should have_link("Sign Out", href: signout_path) }
       it { should_not have_link("Sign In", href: signin_path)}
     end
