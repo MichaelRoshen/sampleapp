83e371835f9adb33cd1b7848fe2611f886344ca4
MichaelRoshen <michael_roshen@163.com>
2013-05-19 18:18:32 +0800

    better redirect_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,9 +15,9 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(params[:user])
-       flash[:success] = "Profile updated"
-       sign_in @user
-       redirect_to @user
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
@@ -37,7 +37,10 @@ class UsersController < ApplicationController
   private
   
   def signed_in_user
-    redirect_to signin_path, notice: "Please Sign in" unless signed_in?
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please Sign in"
+    end
   end
 
   def correct_user
