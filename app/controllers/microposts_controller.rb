de59fb69a05cb7d3bc8126db17585337e8043b6e
MichaelRoshen <michael_roshen@163.com>
2013-06-15 13:23:03 +0800

    add link to delete micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_filter :signed_in_user
+  before_filter :signed_in_user,only: [:create, :destroy]
+
   before_filter :correct_user, only: :destroy
 
   def index
@@ -24,9 +25,10 @@ class MicropostsController < ApplicationController
 
   private
 
-    def correct_user
-      @micropost = current_user.microposts.find_by_id(params[:id])
-      redirect_to root_url if @micropost.nil?
-    end
+  def correct_user
+    @micropost = current_user.microposts.find_by_id(params[:id])
+  rescue
+    redirect_to root_url
+  end
 
 end
