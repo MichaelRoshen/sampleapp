8af9ec6fdad3cf14c8e11ef38b69ecc3e1759fb4
MichaelRoshen <michael_roshen@163.com>
2013-06-17 23:28:01 +0800

    add relationship

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,7 @@ describe User do
   it { should respond_to(:authenticate)}
   it { should respond_to(:remember_token) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:relationships) }
   it { should respond_to(:feed) }
   it { should be_valid }
 
